fd554222b73f9945e5e80d20295a5a76a198c82a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-25 22:31:25 +0550

    feat(FileRepository.java): add methods to retrieve file by slug and id

--- server/src/main/java/com/bitlegion/server/uploads/FileRepository.java
+++ server/src/main/java/com/bitlegion/server/uploads/FileRepository.java
@@ -1,9 +1,17 @@
 package com.bitlegion.server.uploads;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 public interface FileRepository extends CrudRepository<File, Integer> {
+    @Query("SELECT a FROM File a where a.slug = :slug")
+    public Optional<File> findBySlug(@Param("slug") String slug);
 
+    @Query("SELECT a FROM File a where a.slug = :slug AND a.account.id = :userId")
+    public Optional<File> findBySlugAndUserID(@Param("slug") String slug, @Param("userId") Integer userId);
 }
