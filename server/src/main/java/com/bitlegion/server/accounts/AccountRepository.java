5e7256158c579f11140320c3c6a4777aa439bdc6
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-25 21:51:27 +0550

    feat(AccountRepository.java, UserRepository.java): rename file

---
+++ server/src/main/java/com/bitlegion/server/accounts/AccountRepository.java
@@ -0,0 +1,19 @@
+package com.bitlegion.server.accounts;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface AccountRepository extends CrudRepository<Account, Integer> {
+    @Query("SELECT a FROM Account a where a.email = :email")
+    public Optional<Account> findByEmail(@Param("email") String email);
+
+    @Query("SELECT a FROM Account a where a.name = :name")
+    public Optional<Account> findByName(@Param("name") String name);
+
+}
