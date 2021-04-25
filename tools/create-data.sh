1a7a260ff3f748af0e47cde9fe4be0994b9dab2a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-25 22:50:53 +0550

    fix(create-data.sh): use BASE_URL variable, add echo

--- tools/create-data.sh
+++ tools/create-data.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+BASE_URL="http://localhost:8000"
 TOTAL=10
 for i in $(seq 1 $TOTAL);
 do
@@ -7,7 +8,7 @@ do
     EMAIL="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 15 ; echo '')@email.com"
     BIO="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 100 ; echo '')"
 
-    curl -X POST "http://localhost:8000/accounts/new/" -d "name=${USERNAME}&email=${EMAIL}&bio=${BIO}"
+    curl -X POST "${BASE_URL}/accounts/new/" -d "name=${USERNAME}&email=${EMAIL}&bio=${BIO}"
     echo
 done
 
@@ -17,6 +18,7 @@ do
 
     CONTENTS="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10000 ; echo '')"
     echo "${CONTENTS}" >> /tmp/file.txt
-    curl -sSL -i -X POST -H "Content-Type: multipart/form-data" "http://localhost:8000/files/upload/${RANDOM_USER}" -F "file=@/tmp/file.txt"
+    curl -s -i -X POST -H "Content-Type: multipart/form-data" "${BASE_URL}/files/upload/${RANDOM_USER}" -F "file=@/tmp/file.txt" -o /dev/null
     rm /tmp/file.txt
 done
+echo "Uploaded ${TOTAL} files!"
