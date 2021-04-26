09dcf24d00374ff315cee3f4401d56192799f16b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-26 09:25:50 +0550

    feat(ServerApplication.java): add cors origin configurere

--- server/src/main/java/com/bitlegion/server/ServerApplication.java
+++ server/src/main/java/com/bitlegion/server/ServerApplication.java
@@ -3,15 +3,22 @@ package com.bitlegion.server;
 import javax.annotation.Resource;
 
 import com.bitlegion.server.uploads.FilesStorageService;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class ServerApplication implements CommandLineRunner {
 	@Resource
 	FilesStorageService storageService;
+
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
@@ -21,5 +28,17 @@ public class ServerApplication implements CommandLineRunner {
 	public void run(String... arg) throws Exception {
 		// storageService.deleteAll();
 		storageService.init();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				String url = env.getProperty("cors.urls");
+				registry.addMapping("/uploads").allowedOrigins(url);
+				registry.addMapping("/accounts").allowedOrigins(url);
+			}
+		};
 	}
 }
