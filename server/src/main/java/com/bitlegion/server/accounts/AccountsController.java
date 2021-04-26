9000507bd43daff5f6f9651d42a7ca442c646eca
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-26 09:53:26 +0550

    feat(AccountsController.java, FilesController.java): add cors origin filter to url

--- server/src/main/java/com/bitlegion/server/accounts/AccountsController.java
+++ server/src/main/java/com/bitlegion/server/accounts/AccountsController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin
 @Controller // This means that this class is a Controller
 @RequestMapping(path = "/accounts") // This means URLs start with /demo (after Application path)
 public class AccountsController {
--- server/src/main/java/com/bitlegion/server/uploads/FilesController.java
+++ server/src/main/java/com/bitlegion/server/uploads/FilesController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin
 @Controller
 @RequestMapping(path = "/uploads")
 public class FilesController {
