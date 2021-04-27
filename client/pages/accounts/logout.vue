16546bdd9c8acb6c61d432e2ac76d25515a9329e
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-27 11:20:52 +0550

    feat(logout.vue): add page for logout

---
+++ client/pages/accounts/logout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Logged out successfully!</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.localStorage.removeItem('username')
+    window.localStorage.removeItem('userId')
+  },
+}
+</script>
