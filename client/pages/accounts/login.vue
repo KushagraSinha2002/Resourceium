9f4726baa7a4e90c34f770ff6c1af34bdf828dce
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-27 11:22:28 +0550

    feat(login.vue): redirect on when logged in

--- client/pages/accounts/login.vue
+++ client/pages/accounts/login.vue
@@ -29,17 +29,37 @@ export default {
     }
   },
   head: () => ({ title: 'Login' }),
+  mounted() {
+    if (
+      window.localStorage.getItem('username') &&
+      window.localStorage.getItem('userId')
+    ) {
+      this.$addAlert({
+        severity: 'warning',
+        messageHeading: 'Logged In',
+        messageBody: 'You are already logged in',
+        active: true,
+      })
+      this.redirectHome()
+    }
+  },
   methods: {
+    redirectHome() {
+      this.$router.push({ name: 'index' })
+    },
     submitForm() {
       const formData = this.formData
       this.$axios
         .$post('/accounts/login', null, { params: formData })
-        .then((_result) => {
+        .then((result) => {
           this.$addAlert({
             severity: 'info',
             messageHeading: 'Login successful',
             active: true,
           })
+          window.localStorage.setItem('username', result.username)
+          window.localStorage.setItem('userId', result.userId)
+          this.redirectHome()
         })
         .catch((err) => {
           this.$addAlert({
