82ab5e1f54d39ffaf3fc96ec0307b3c01b74554b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-27 09:17:08 +0550

    feat(nuxt.config.js): install alert plugin

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   css: ['~/assets/css/global.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/ignisnents.js'],
+  plugins: ['~/plugins/ignisnents.js', '~/plugins/alerts.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
