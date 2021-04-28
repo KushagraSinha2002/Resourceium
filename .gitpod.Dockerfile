b0cc9b5ed9f8adfe3eac4b2f8ee3fc44b20dd7c6
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-28 22:25:04 +0530

    change gitpod db to postgres

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,8 +1,7 @@
-FROM gitpod/workspace-mysql
+FROM gitpod/workspace-postgres
 
 RUN bash -c "source /home/gitpod/.sdkman/bin/sdkman-init.sh && sdk install java 15.0.2.7.1-amzn"
 
-ENV MYSQL_ROOT_PASSWORD=password
-ENV MYSQL_PASSWORD=password
-ENV MYSQL_USER=username
-ENV MYSQL_DATABASE=server_db
+ENV DB_PASSWORD=password
+ENV DB_USER=username
+ENV DB_DATABASE=server_db
--- tools/init-db.sh
+++ tools/init-db.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-PASSWORD="${1:-$MYSQL_PASSWORD}"
+PASSWORD="${1:-$DB_PASSWORD}"
 
-MAIN_DB="${2:-$MYSQL_DATABASE}"
+MAIN_DB="${2:-$DB_DATABASE}"
 
-DB_USER="${3:-$MYSQL_USER}"
+DB_USER="${3:-$DB_USER}"
 
-mysql -u root <<MYSQL_SCRIPT
-CREATE DATABASE $MAIN_DB;
-
-CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MAIN_DB}.* TO '${DB_USER}'@'localhost';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
+cat << EOF | su - postgres -c psql
+-- Create the database user:
+CREATE ROLE "$DB_USER" WITH LOGIN CREATEDB ENCRYPTED PASSWORD '$PASSWORD';
+-- Create the database:
+CREATE DATABASE "$MAIN_DB" WITH OWNER="$DB_USER"
+                                  LC_COLLATE="en_US.utf8"
+                                  LC_CTYPE="en_US.utf8"
+                                  ENCODING="UTF8"
+                                  TEMPLATE=template0;
+-- Grant necessary privileges
+GRANT ALL PRIVILEGES ON DATABASE "$MAIN_DB" TO "$DB_USER";
+EOF
