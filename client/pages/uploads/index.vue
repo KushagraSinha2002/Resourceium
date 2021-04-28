2ab275b87e6261134b0ab9a18b91388586e79a65
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-28 20:22:15 +0550

    feat(index.vue): add simple page to display and download all uploads

--- client/pages/uploads/index.vue
+++ client/pages/uploads/index.vue
@@ -1,103 +1,37 @@
 <template>
   <div>
-    <form
-      enctype="multipart/form-data"
-      novalidate
-      @submit.prevent="handleSubmit"
-    >
-      <h1>Upload files</h1>
-      <div class="dropbox">
-        <input
-          id="files"
-          ref="files"
-          type="file"
-          multiple
-          name="files"
-          :disabled="isSaving"
-          @change="handleFileChange"
-        />
-        <button type="submit" class="bg-blue-300 border border-black">
-          Upload
-        </button>
-      </div>
-    </form>
+    <div>All uploads</div>
     <div>
       <div v-for="(file, index) in files" :key="index">
-        {{ file.name }} - {{ file.size | getFileSize }}
+        <div class="hover:underline">
+          <a :href="getFileUrl(file)">{{ file.name }}</a>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import filesize from 'filesize'
-
-const STATUS_INITIAL = 0
-const STATUS_SAVING = 1
-const STATUS_SUCCESS = 2
-const STATUS_FAILED = 3
+import { resolveURL, withQuery } from 'ufo'
 
 export default {
-  filters: {
-    getFileSize(value) {
-      return filesize(value)
-    },
-  },
   data() {
     return {
-      uploadedFiles: [],
-      currentStatus: null,
-      fileCount: 0,
       files: [],
+      baseUrl: process.env.baseUrl,
     }
   },
-  computed: {
-    isInitial() {
-      return this.currentStatus === STATUS_INITIAL
-    },
-    isSaving() {
-      return this.currentStatus === STATUS_SAVING
-    },
-    isSuccess() {
-      return this.currentStatus === STATUS_SUCCESS
-    },
-    isFailed() {
-      return this.currentStatus === STATUS_FAILED
-    },
-  },
   mounted() {
-    this.reset()
+    this.$axios.$get('/uploads/all').then((resp) => {
+      this.files = resp
+    })
   },
   methods: {
-    reset() {
-      // reset form to initial state
-      this.currentStatus = STATUS_INITIAL
-      this.uploadedFiles = []
-    },
-    save(formData) {
-      // upload data to the server
-      this.currentStatus = STATUS_SAVING
-      this.$uploadFile(window.localStorage.getItem('userId'), formData)
-      this.currentStatus = STATUS_SUCCESS
-    },
-    handleFileChange() {
-      this.files = this.$refs.files.files
-    },
-    handleSubmit() {
-      const fileList = this.$refs.files.files
-      // handle file changes
-      const formData = new FormData()
-
-      if (!fileList.length) return
-
-      // append the files to FormData
-      for (const propName in fileList) {
-        if (typeof fileList[propName] === 'object') {
-          formData.append('files', fileList[propName], fileList[propName].name)
-        }
-      }
-      // save it
-      this.save(formData)
+    getFileUrl(file) {
+      return withQuery(resolveURL(this.baseUrl, 'uploads', 'download'), {
+        userId: String(file.account.id),
+        filename: file.slug,
+      })
     },
   },
 }
