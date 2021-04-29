8a51d55f948ec9d174022270dca397a4778b13b0
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-29 23:05:46 +0550

    feat(login.vue): finalize login page layout

--- client/pages/accounts/login.vue
+++ client/pages/accounts/login.vue
@@ -1,20 +1,60 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <form class="flex flex-col max-w-sm space-y-5" @submit.prevent="submitForm">
-      <base-input-box v-model="formData.name" name="username"></base-input-box>
-      <base-input-box
-        v-model="formData.password"
-        name="password"
-        input-type="password"
-      ></base-input-box>
-      <button
-        type="submit"
-        class="flex justify-center py-2 space-x-3 rounded-lg focus:outline-none bg-lime-600 ring-blue-200 ring"
+  <div
+    class="container flex flex-wrap-reverse items-center h-screen mx-auto bg-cream-white place-content-center sm:place-content-evenly"
+  >
+    <div class="w-full px-6 sm:w-1/2 sm:px-0">
+      <form
+        class="flex flex-col font-poppins max-w-sm sm:max-w-[25rem] md:max-w-md mx-auto space-y-5 md:text-xl sm:text-lg"
+        @submit.prevent="submitForm"
       >
-        <ig-icon name="check" no-color class="text-black"></ig-icon>
-        <div>Submit</div>
-      </button>
-    </form>
+        <div
+          class="mt-10 mb-5 text-3xl font-bold sm:text-4xl sm:mb-10 font-poppins text-dark-black"
+        >
+          Please Sign In.
+        </div>
+        <input
+          id="username"
+          v-model="formData.name"
+          type="text"
+          placeholder="Resourceium username"
+          class="px-4 py-2 border rounded-lg appearance-none border-dark-black border-opacity-40 focus:outline-none focus:border-blue-300"
+        />
+        <label for="username" class="hidden">Username</label>
+        <input
+          id="password"
+          v-model="formData.password"
+          type="text"
+          placeholder="Password"
+          class="px-4 py-2 border rounded-lg appearance-none border-dark-black border-opacity-40 focus:outline-none focus:border-blue-300"
+        />
+        <label for="password" class="hidden">Password</label>
+        <button
+          type="submit"
+          class="flex justify-center py-2 space-x-3 transition duration-300 transform rounded-lg hover:scale-105 sm:py-3 focus:outline-none bg-primary ring-black ring-1"
+        >
+          <div class="text-white">Login</div>
+        </button>
+        <div class="text-sm sm:text-base font-styled text-primary">
+          <div>
+            <NuxtLink :to="{ name: 'index' }" class="hover:underline">
+              Don't have a Resourceium account? Create one here.
+            </NuxtLink>
+          </div>
+          <div>
+            <NuxtLink :to="{ name: 'index' }" class="hover:underline">
+              Forgot your Resourceium username or password?
+            </NuxtLink>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="w-full sm:w-1/2">
+      <img
+        :src="require('~/assets/images/logo.png')"
+        alt="Resourceium logo"
+        class="h-48 mx-auto sm:h-80 md:h-96 xl:h-[30rem]"
+      />
+    </div>
   </div>
 </template>
 
