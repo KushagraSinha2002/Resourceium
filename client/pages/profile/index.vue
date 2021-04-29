9683510e1dee5d092aedeffe14bc036c2ba51c3e
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-04-29 15:16:34 +0550

    feat(index.vue): show profile details

---
+++ client/pages/profile/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>Profile</div>
+    <div>{{ user }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: null,
+    }
+  },
+  mounted() {
+    this.$axios
+      .$get('/accounts/' + window.localStorage.getItem('userId'))
+      .then((resp) => {
+        this.user = resp
+      })
+  },
+}
+</script>
