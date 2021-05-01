0966ebff64fb98f0f0d8f33ee7e3c31d0d2180ea
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-01 18:03:52 +0550

    feat(default.vue): apply blur to background for alerts

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,15 +1,43 @@
 <template>
-  <div :class="mode !== 'production' ? 'debug-screens' : ''">
-    <alerts-container></alerts-container>
-    <nav-bar></nav-bar>
-    <Nuxt />
+  <div>
+    <transition name="alert">
+      <alert v-if="show"></alert>
+    </transition>
+    <div
+      :class="{
+        'debug-screens': mode !== 'production',
+        'filter blur-[2px]  sm:blur-[1px]': show,
+      }"
+    >
+      <nav-bar></nav-bar>
+      <Nuxt />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data: () => ({
     mode: process.env.NODE_ENV,
   }),
+  computed: {
+    ...mapState({
+      show: (state) => state.alerts.show,
+    }),
+  },
 }
 </script>
+
+<style scoped>
+.alert-enter-active,
+.alert-leave-active {
+  transition: opacity 0.2s;
+}
+
+.alert-enter,
+.alert-leave-to {
+  opacity: 0;
+}
+</style>
