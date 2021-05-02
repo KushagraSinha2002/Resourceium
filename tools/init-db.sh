82e65cdd4688a17d54bd944fed7aab5157bb88e5
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-02 16:25:37 +0550

    build(init-db.sh): add basic logging

--- tools/init-db.sh
+++ tools/init-db.sh
@@ -8,6 +8,8 @@ DB_USER="${3:-$DB_USER}"
 
 ROOT_USER="${4:-gitpod}"
 
+printr "Creating server database"
+
 psql -U "${ROOT_USER}" << SQL_COMMANDS
 -- Database for the server->
 -- Create the database user:
@@ -22,6 +24,10 @@ CREATE DATABASE "$MAIN_DB" WITH OWNER="$DB_USER"
 GRANT ALL PRIVILEGES ON DATABASE "$MAIN_DB" TO "$DB_USER";
 SQL_COMMANDS
 
+printr "Successfully created server database"
+
+printr "Creating storage database"
+
 psql -U "${ROOT_USER}" << SQL_COMMANDS
 -- Database for the storage server->
 CREATE ROLE "$DJANGO_DATABASE_USER" WITH LOGIN CREATEDB ENCRYPTED PASSWORD '$DJANGO_DATABASE_PASSWORD';
@@ -34,3 +40,5 @@ CREATE DATABASE "$DJANGO_DATABASE_NAME" WITH OWNER="$DJANGO_DATABASE_USER"
 -- Grant necessary privileges
 GRANT ALL PRIVILEGES ON DATABASE "$DJANGO_DATABASE_NAME" TO "$DJANGO_DATABASE_USER";
 SQL_COMMANDS
+
+printr "Successfully created storage database"
