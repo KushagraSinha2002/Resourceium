03d61b5dc84ce5e5007a00b61f14f07cbdf3b55c
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-02 23:49:08 +0550

    feat(views.py): save upload object

--- storage/files/views.py
+++ storage/files/views.py
@@ -3,11 +3,16 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from . import models
+
 
 class FileUploadView(APIView):
     parser_classes = (MultiPartParser,)
 
     def post(self, request, format=None):
         file_obj = request.FILES["file"]
-        # print(dir(file_obj))
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        obj = models.Upload.objects.create(
+            file=file_obj, user_id=request.POST.get("user_id")[0]
+        )
+        obj.save()
+        return Response(data={"url": obj.file.url}, status=status.HTTP_200_OK)
