e98a1e9d5bb4ceae159b3c62d398960b1f77ec6e
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-02 23:48:57 +0550

    feat(models.py): add user_id field to upload model

--- storage/files/models.py
+++ storage/files/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+def get_upload_directory(instance, filename):
+    return os.path.join("uploads", instance.user_id, f"slug--{uuid.uuid4()}--{filename}")
+
+
+class Upload(models.Model):
+    file = models.FileField(
+        help_text=_("The actual file that is uploaded"), upload_to=get_upload_directory
+    )
+    user_id = models.IntegerField(help_text=_("The id of the user this file belongs to"))
+
+    def __str__(self):
+        return f"{self.user_id}"
