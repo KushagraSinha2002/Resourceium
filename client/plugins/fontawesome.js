7a856861452054ec3ca00c16aea47bbaa4d50f5b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-02 14:25:25 +0550

    feat(fontawesome.js): add plugin to display brand icons

---
+++ client/plugins/fontawesome.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {} from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import {
+  faInstagram,
+  faTwitter,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
+
+// This is important, we are going to let Nuxt.js worry about the CSS
+config.autoAddCss = false
+
+// You can add your icons directly in this plugin. See other examples for how you
+// can add other styles or just individual icons.
+library.add(faEnvelope, faInstagram, faTwitter, faWhatsapp)
+
+// Register the component globally
+Vue.component('FontAwesomeIcon', FontAwesomeIcon)
