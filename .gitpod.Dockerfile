aa7010c818bd7b20ce0c29fb1d9c8096043d42af
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-02 16:24:03 +0550

    build: setup for storage project

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -5,3 +5,6 @@ RUN bash -c "source /home/gitpod/.sdkman/bin/sdkman-init.sh && sdk install java 
 ENV DB_PASSWORD=password
 ENV DB_USER=username
 ENV DB_DATABASE=server_db
+ENV DJANGO_DATABASE_NAME=website-db
+ENV DJANGO_DATABASE_USER=admin-user
+ENV DJANGO_DATABASE_PASSWORD=admin-password
--- tools/init-db.sh
+++ tools/init-db.sh
@@ -9,6 +9,7 @@ DB_USER="${3:-$DB_USER}"
 ROOT_USER="${4:-gitpod}"
 
 psql -U "${ROOT_USER}" << SQL_COMMANDS
+-- Database for the server->
 -- Create the database user:
 CREATE ROLE "$DB_USER" WITH LOGIN CREATEDB ENCRYPTED PASSWORD '$PASSWORD';
 -- Create the database:
@@ -19,4 +20,17 @@ CREATE DATABASE "$MAIN_DB" WITH OWNER="$DB_USER"
                                   TEMPLATE=template0;
 -- Grant necessary privileges
 GRANT ALL PRIVILEGES ON DATABASE "$MAIN_DB" TO "$DB_USER";
+SQL_COMMANDS
+
+psql -U "${ROOT_USER}" << SQL_COMMANDS
+-- Database for the storage server->
+CREATE ROLE "$DJANGO_DATABASE_USER" WITH LOGIN CREATEDB ENCRYPTED PASSWORD '$DJANGO_DATABASE_PASSWORD';
+-- Create the database:
+CREATE DATABASE "$DJANGO_DATABASE_NAME" WITH OWNER="$DJANGO_DATABASE_USER"
+                                  LC_COLLATE="en_US.utf8"
+                                  LC_CTYPE="en_US.utf8"
+                                  ENCODING="UTF8"
+                                  TEMPLATE=template0;
+-- Grant necessary privileges
+GRANT ALL PRIVILEGES ON DATABASE "$DJANGO_DATABASE_NAME" TO "$DJANGO_DATABASE_USER";
 SQL_COMMANDS
