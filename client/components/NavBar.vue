b7f3fec53d62c67c6816ceea65af962bfd298d3b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-02 12:00:36 +0550

    feat(NavBar.vue): add basic navbar and logic

--- client/components/NavBar.vue
+++ client/components/NavBar.vue
@@ -1,33 +1,19 @@
 <template>
-  <div class="fixed right-0 hidden px-3 py-4 font-bold font-styled sm:block">
-    <NuxtLink
-      v-for="(link, index) in links"
-      :key="index"
-      :to="{ name: link.path }"
-      class="grid items-center grid-cols-2 space-x-5 hover:underline hover:text-emerald-500"
-    >
-      <div class="text-right">{{ link.name }}</div>
-      <ig-icon
-        no-color
-        size="sm"
-        :name="link.icon"
-        class="text-black"
-      ></ig-icon>
-    </NuxtLink>
+  <div
+    class="flex w-full px-3 py-4 font-bold text-white font-styled bg-light-black"
+  >
+    <button @click="toggleSidebarMutation()">Toggle</button>
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
-  data() {
-    return {
-      links: [
-        { name: 'Home', path: 'index', icon: 'home' },
-        { name: 'Register', path: 'accounts-register', icon: 'key' },
-        { name: 'Login', path: 'accounts-login', icon: 'lock' },
-        { name: 'Logout', path: 'accounts-logout', icon: 'unlock' },
-      ],
-    }
+  methods: {
+    ...mapMutations({
+      toggleSidebarMutation: 'sidebar/toggleSidebar',
+    }),
   },
 }
 </script>
