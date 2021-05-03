26dea82a3c7a7f4195eda24fd3b84e22268fb514
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-03 09:08:04 +0550

    fix(urls.py): change url to new schema

--- storage/files/urls.py
+++ storage/files/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("upload/", views.FileUploadView.as_view(), name="upload_file"),
+    path(
+        "upload/<int:user_id>/<int:folder_id>",
+        views.FileUploadView.as_view(),
+        name="upload_file",
+    ),
 ]
