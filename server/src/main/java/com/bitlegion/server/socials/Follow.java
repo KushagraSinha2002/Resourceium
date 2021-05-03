94d60f96027b608a648baa74d75cb6ebf3c22dd5
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-03 08:50:17 +0550

    feat(Follow.java): add foreign key relationship to account

--- server/src/main/java/com/bitlegion/server/socials/Follow.java
+++ server/src/main/java/com/bitlegion/server/socials/Follow.java
@@ -1,43 +1,28 @@
 package com.bitlegion.server.socials;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.bitlegion.server.accounts.Account;
 
 @Entity
 public class Follow {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    
-    private String followers;
 
-    private String following;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "follower_id", referencedColumnName = "id")
+    private Account follower;
 
- //   @ManyToOne(cascade = CascadeType.ALL)
- //   @JoinColumn(name = "account_id", referencedColumnName = "id")
- //   private Account account;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "following_id", referencedColumnName = "id")
+    private Account following;
 
-    public String getFollowers() {
-        return followers;
-    }
-
-    public String getFollowing() {
-        return following;
-    }
-
-    public void setFollowers(String followers) {
-        this.followers = followers;
-    }
-
-    public void setFollowing(String following) {
-        this.following = following;
-    }
-
-    @Override
-    public String toString() {
-        return "{" + " followers='" + getFollowers() + "'" + ", following='" + getFollowing() + "'" + "}";
-    }
 }
