88d65fb82cbf75f5473d1de6117ffcb547c97f70
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-03 09:07:31 +0550

    feat(0003_auto_20210503_0844.py): add folder_id and name fields

--- storage/files/models.py
+++ storage/files/models.py
@@ -1,19 +1,29 @@
 import os
-import uuid
 
 from django.db import models
 from django.utils.translation import gettext as _
 
 
 def get_upload_directory(instance, filename):
-    return os.path.join("uploads", instance.user_id, f"slug--{uuid.uuid4()}--{filename}")
+    return os.path.join(
+        "uploads",
+        f"user--{instance.user_id}",
+        f"folder--{instance.folder_id}",
+        f"{filename}",
+    )
 
 
 class Upload(models.Model):
     file = models.FileField(
-        help_text=_("The actual file that is uploaded"), upload_to=get_upload_directory
+        help_text=_("The actual file that is uploaded."), upload_to=get_upload_directory
     )
-    user_id = models.IntegerField(help_text=_("The id of the user this file belongs to"))
+    user_id = models.IntegerField(
+        help_text=_("The id of the user this file belongs to.")
+    )
+    folder_id = models.IntegerField(
+        help_text=_("The id of the folder this file belongs to.")
+    )
+    name = models.TextField(help_text=_("The name of the file that was uploaded."))
 
     def __str__(self):
         return f"{self.user_id}"
