5a97a32aa9798093f1f49b9e51d3209244fe47c9
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-03 09:09:21 +0550

    feat(views.py): save file and name

--- storage/files/views.py
+++ storage/files/views.py
@@ -9,10 +9,13 @@ from . import models
 class FileUploadView(APIView):
     parser_classes = (MultiPartParser,)
 
-    def post(self, request, format=None):
+    def post(self, request, format=None, *args, **kwargs):
         file_obj = request.FILES["file"]
         obj = models.Upload.objects.create(
-            file=file_obj, user_id=request.POST.get("user_id")[0]
+            file=file_obj,
+            user_id=kwargs.get("user_id"),
+            folder_id=kwargs.get("folder_id"),
+            name=file_obj.name,
         )
         obj.save()
         return Response(data={"url": obj.file.url}, status=status.HTTP_200_OK)
