9b61a3ed43a95f22b0a93c96ff8d9e3d6ed3b107
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-03 10:18:54 +0550

    feat(FileRepository.java): change method to find by storageID

--- server/src/main/java/com/bitlegion/server/uploads/FileRepository.java
+++ server/src/main/java/com/bitlegion/server/uploads/FileRepository.java
@@ -9,11 +9,13 @@ import org.springframework.data.repository.query.Param;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 public interface FileRepository extends CrudRepository<Document, Integer> {
-    @Query("SELECT a FROM Document a where a.url = :url")
-    public Optional<Document> findBySlug(@Param("url") String url);
+    @Query("SELECT a FROM Document a where a.storageID = :storageID")
+    public Optional<Document> findByStorageID(@Param("storageID") Long storageID);
 
-    // @Query("SELECT a FROM Document a where a.url = :url AND a.account.id =
+    // @Query("SELECT a FROM Document a where a.storageID = :storageID AND
+    // a.account.id =
     // :userId")
-    // public Optional<Document> findBySlugAndUserID(@Param("url") String url,
+    // public Optional<Document> findBySlugAndUserID(@Param("storageID") String
+    // storageID,
     // @Param("userId") Integer userId);
 }
