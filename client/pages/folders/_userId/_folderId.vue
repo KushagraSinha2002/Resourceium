725d3962e3544aa0c58ca372164b04cbbf163444
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-05 12:33:33 +0550

    feat(_folderId.vue): add page to display a single folder

---
+++ client/pages/folders/_userId/_folderId.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ folder }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      folder: null,
+    }
+  },
+  async mounted() {
+    this.folder = await this.getFolderDetails()
+  },
+  methods: {
+    async getFolderDetails() {
+      const params = this.$route.params
+      const resp = await this.$axios.$get(`/folders/folder/${params.folderId}`)
+      return resp
+    },
+  },
+}
+</script>
