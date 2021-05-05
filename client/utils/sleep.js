44f9f74ba1d8764bf893e00ba88b680bfd9aab86
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-05 09:54:13 +0550

    docs(sleep.js): add comments to explain usage

--- client/utils/sleep.js
+++ client/utils/sleep.js
@@ -1,4 +1,6 @@
+/* Use this in any async context by invoking it as `await sleep(durationInMilliseconds)` */
 export function sleep(time) {
-  // Use this in any async context by invoking it as `await sleep(durationInMilliseconds)`
+  // eslint-disable-next-line no-console
+  console.log(`Sleeping main thread for ${time} milliseconds`)
   return new Promise((resolve) => setTimeout(resolve, time))
 }
