25363b2b764b5dd9c2d2db0dc09a1c72c2ca6698
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-05 14:24:49 +0550

    feat(Header.vue): compoenent to separate sidebar header

---
+++ client/components/sidebar/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="flex items-center px-5 py-2 mb-5 space-x-4 border-blue-500 from-cream-white bg-gradient-to-tr to-steel-300"
+  >
+    <img
+      :src="require('~/assets/images/robot-profile.png')"
+      alt="profile image"
+      class="w-12 h-12"
+    />
+
+    <div class="truncate">
+      <div
+        class="text-lg font-semibold text-gray-900 uppercase font-styled-code"
+      >
+        {{ username }}
+      </div>
+      <div class="text-xs text-warm-gray-700 font-poppins">{{ email }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: null,
+      email: null,
+    }
+  },
+  async mounted() {
+    const resp = await this.$axios.$get(
+      `/accounts/${window.localStorage.getItem('userId')}`
+    )
+    this.username = resp.username
+    this.email = resp.email
+  },
+}
+</script>
