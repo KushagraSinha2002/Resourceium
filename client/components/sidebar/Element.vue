20704888d81409a975425559a28ae27fbb84deb5
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-05 14:25:00 +0550

    feat(Element.vue): compoenent to separate actual sidebar element

---
+++ client/components/sidebar/Element.vue
@@ -0,0 +1,32 @@
+<template>
+  <NuxtLink :to="{ name: link.path }" class="flex items-center px-4 space-x-5">
+    <ig-icon
+      size="lg"
+      :name="link.icon"
+      no-color
+      class="text-cream-white"
+    ></ig-icon>
+    <div
+      class="text-xl text-right uppercase hover:underline hover:text-blue-400 font-poppins text-warm-gray-200"
+    >
+      {{ link.name }}
+    </div>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.nuxt-link-exact-active.nuxt-link-active {
+  @apply border-l-4 border-lime-300;
+}
+</style>
