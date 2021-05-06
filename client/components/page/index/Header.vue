c4b7b36b563bab78cd54b48c5350c774d7c8dea1
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-06 13:21:15 +0550

    fix(Header.vue): chage spacing layout

--- client/components/page/index/Header.vue
+++ client/components/page/index/Header.vue
@@ -1,26 +1,28 @@
 <template>
-  <div
-    class="flex-none w-full py-3 bg-[#F5F5F7] flex items-center justify-around"
-  >
-    <NuxtLink
-      :to="{ name: 'index' }"
-      class="font-thin font-poppins hover:underline"
+  <div class="flex-none w-full py-3 bg-[#F5F5F7] flex">
+    <div
+      class="flex items-center justify-between w-full mx-auto sm:w-9/12 2xl:w-8/12"
     >
-      Why Resourceium?
-    </NuxtLink>
-    <div class="flex items-center space-x-3 font-semibold sm:space-x-5">
       <NuxtLink
-        class="text-primary font-poppins hover:underline"
-        :to="{ name: 'accounts-login' }"
+        :to="{ name: 'index' }"
+        class="font-thin font-poppins hover:underline"
       >
-        Sign In
+        Why Resourceium?
       </NuxtLink>
-      <NuxtLink
-        class="text-primary font-poppins hover:underline"
-        :to="{ name: 'accounts-register' }"
-      >
-        Sign Up
-      </NuxtLink>
+      <div class="flex items-center space-x-3 font-semibold sm:space-x-5">
+        <NuxtLink
+          class="text-primary font-poppins hover:underline"
+          :to="{ name: 'accounts-login' }"
+        >
+          Sign In
+        </NuxtLink>
+        <NuxtLink
+          class="text-primary font-poppins hover:underline"
+          :to="{ name: 'accounts-register' }"
+        >
+          Sign Up
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
