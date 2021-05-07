7b032f0fedd19a9ad671664d67ea33a5f97ca9bc
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-07 10:10:22 +0550

    feat(Alert.vue): change appearance

--- client/components/Alert.vue
+++ client/components/Alert.vue
@@ -2,18 +2,20 @@
   <div
     class="absolute inset-0 z-30 flex items-center justify-center bg-white bg-opacity-30"
   >
-    <div class="relative flex items-center px-5 py-2 space-x-5 border">
+    <div
+      class="relative flex items-center p-3 space-x-5 bg-[#ADA8A9] rounded-b-md"
+    >
       <div
         :style="{ width: `${width}%` }"
-        class="absolute inset-x-0 top-0 h-0.5 bg-red-600"
+        class="absolute inset-x-0 top-0 h-1 bg-black"
       ></div>
       <ig-icon
         :name="getIconName"
-        size="xl"
-        no-color
-        :class="getIconColor"
+        class="h-10"
+        no-size
+        variant="black"
       ></ig-icon>
-      <div>{{ message }}</div>
+      <div class="text-lg font-semibold font-poppins">{{ message }}</div>
     </div>
   </div>
 </template>
@@ -37,15 +39,9 @@ export default {
     }),
     getIconName() {
       if (this.type === 'info') return 'info'
-      if (this.type === 'danger') return 'x-octagon'
+      if (this.type === 'danger') return 'alert-triangle'
       if (this.type === 'success') return 'check'
       return 'alert-triangle'
-    },
-    getIconColor() {
-      if (this.type === 'info') return 'text-blue-500'
-      if (this.type === 'danger') return 'text-red-500'
-      if (this.type === 'success') return 'text-lime-green-500'
-      return 'text-yellow-600'
     },
   },
   async mounted() {
