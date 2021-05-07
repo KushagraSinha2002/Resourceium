3eb3616c591b190eca861b4a7a913d6738440085
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-07 08:47:10 +0550

    feat(Footer.vue): add component to display logout button

---
+++ client/components/sidebar/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLink
+    :to="{ name: 'accounts-logout' }"
+    class="flex items-center justify-center px-2 py-1 space-x-8 md:py-3 rounded-t-xl from-red-700 bg-gradient-to-t to-sunset-600"
+  >
+    <ig-icon name="power" variant="black" size="lg"></ig-icon>
+    <div class="text-xl font-bold font-poppins">Logout</div>
+  </NuxtLink>
+</template>
