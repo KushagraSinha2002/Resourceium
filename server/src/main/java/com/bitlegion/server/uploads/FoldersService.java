6feca997cc88828e5f5afb8121780e52a67411d5
Surbhi Goel <surbhigoel02@gmail.com>
2021-05-07 14:57:59 +0000

    controller file to service file

--- server/src/main/java/com/bitlegion/server/uploads/FoldersService.java
+++ server/src/main/java/com/bitlegion/server/uploads/FoldersService.java
@@ -17,7 +17,7 @@ import com.bitlegion.server.accounts.TokenChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,10 +37,10 @@ import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-@Controller
+@Service
 @RequestMapping(path = "/folders")
 @CrossOrigin
-public class FoldersController {
+public class FoldersService {
 
     @Autowired
     private TokenChecker tokenChecker;
