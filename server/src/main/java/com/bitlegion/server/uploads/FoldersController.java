30c32fe5a13357e41b7436741c4a10fad37a00c3
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-07 15:16:34 +0550

    feat(FoldersController.java): add endpoint to upload file to folder

--- server/src/main/java/com/bitlegion/server/uploads/FoldersController.java
+++ server/src/main/java/com/bitlegion/server/uploads/FoldersController.java
@@ -1,11 +1,16 @@
 package com.bitlegion.server.uploads;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.bitlegion.server.accounts.Account;
+import com.bitlegion.server.accounts.AccountRepository;
 import com.bitlegion.server.accounts.Token;
 import com.bitlegion.server.accounts.TokenChecker;
 
@@ -19,6 +24,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.FileBody;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 @Controller
 @RequestMapping(path = "/folders")
@@ -32,8 +49,12 @@ public class FoldersController {
     private FolderRepository folderRepository;
 
     @Autowired
+    private AccountRepository userRepository;
 
-    @GetMapping("/folders")
+    @Autowired
+    private FileRepository fileRepository;
+
+    @GetMapping("/user-folders")
     public ResponseEntity<Iterable<Folder>> getListFolders(HttpServletRequest request) {
         try {
             Token token = tokenChecker.checkAndReturnTokenOrRaiseException(request);
@@ -66,6 +87,53 @@ public class FoldersController {
             folder.setAccount(token.getAccount());
             folderRepository.save(folder);
             return ResponseEntity.status(HttpStatus.OK).body(folder);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/upload-file/{folderID}")
+    public ResponseEntity<Document> uploadFile(HttpServletRequest request, @RequestParam MultipartFile file,
+            @PathVariable Integer folderID) {
+        try {
+
+            Token token = tokenChecker.checkAndReturnTokenOrRaiseException(request);
+            Account account = token.getAccount();
+            Folder folder = folderRepository.findById(folderID).get();
+            Path filepath = Paths.get(System.getProperty("java.io.tmpdir"), file.getOriginalFilename());
+            try {
+                file.transferTo(filepath);
+            } catch (Exception e) {
+                System.out.println("error: " + e.getMessage());
+                return ResponseEntity.badRequest().build();
+            }
+            try {
+                String URL = System.getenv("STORAGE_SERVER") + "files/upload/" + account.getId().toString() + "/"
+                        + folder.getId();
+                File diskFile = new File(filepath.toString());
+                CloseableHttpClient client = HttpClients.createDefault();
+                HttpPost post = new HttpPost(URL);
+                HttpEntity entity = MultipartEntityBuilder.create().addPart("file", new FileBody(diskFile)).build();
+                post.setEntity(entity);
+                CloseableHttpResponse response = client.execute(post);
+                HttpEntity entityResponse = response.getEntity();
+                String result = EntityUtils.toString(entityResponse);
+                JSONParser parser = new JSONParser();
+                JSONObject json = (JSONObject) parser.parse(result);
+                Document document = new Document();
+                document.setName(file.getOriginalFilename());
+                document.setStorageID((Long) json.get("storageID"));
+                document.setFolder(folder);
+                fileRepository.save(document);
+                folder.setDocument(document);
+                userRepository.save(account);
+                folderRepository.save(folder);
+                return ResponseEntity.status(HttpStatus.OK).body(document);
+            } catch (Exception e) {
+                System.out.println("error: " + e.getMessage());
+                return ResponseEntity.badRequest().build();
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return ResponseEntity.badRequest().build();
