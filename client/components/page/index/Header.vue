49c23fc5b8c509a2a3cacc434cf23dcb7b92e2e3
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-08 19:33:28 +0550

    fix(Header.vue): add padding for mobile screens

--- client/components/page/index/Header.vue
+++ client/components/page/index/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex-none w-full py-3 bg-[#F5F5F7] flex">
     <div
-      class="flex items-center justify-between w-full mx-auto sm:w-9/12 2xl:w-8/12"
+      class="flex items-center justify-between w-full px-4 mx-auto sm:w-9/12 2xl:w-8/12"
     >
       <NuxtLink
         :to="{ name: 'index' }"
