3228f36f91d8ade95de3ccc3a91763d4c073f5d7
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-08 20:14:06 +0550

    feat(Sleeper.java): add java class to sleep thread

---
+++ server/src/main/java/com/bitlegion/server/core/Sleeper.java
@@ -0,0 +1,22 @@
+package com.bitlegion.server.core;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sleeper {
+
+    @Value("${spring-dev-mode}")
+    private Boolean devMode;
+
+    public void pause(int ms) {
+        if (devMode) {
+            try {
+                System.out.println("Sleeping for " + ms + " milliseconds");
+                Thread.sleep(ms);
+            } catch (InterruptedException e) {
+                System.err.format("IOException: %s%n", e);
+            }
+        }
+    }
+}
