82e5d7f2373a25d6ebf9ddf95e347f38b53c070f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-08 11:35:52 +0550

    feat(ServerApplication.java, FileUploadExceptionAdvice.java): remove file

--- server/src/main/java/com/bitlegion/server/ServerApplication.java
+++ server/src/main/java/com/bitlegion/server/ServerApplication.java
@@ -1,25 +1,13 @@
 package com.bitlegion.server;
 
-import javax.annotation.Resource;
-
-import com.bitlegion.server.uploads.FilesStorageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class ServerApplication implements CommandLineRunner {
-	@Resource
-	FilesStorageService storageService;
+public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... arg) throws Exception {
-		// storageService.deleteAll();
-		storageService.init();
 	}
 
 }
