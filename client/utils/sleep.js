a2e5f56c0b201c749723150d88d4ec5888c1afdf
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-10 07:28:00 +0550

    fix(sleep.js): change to console.info

--- client/utils/sleep.js
+++ client/utils/sleep.js
@@ -1,6 +1,6 @@
 /* Use this in any async context by invoking it as `await sleep(durationInMilliseconds)` */
 export function sleep(time) {
   // eslint-disable-next-line no-console
-  console.log(`Sleeping main thread for ${time} milliseconds`)
+  console.info(`Sleeping main thread for ${time} milliseconds`)
   return new Promise((resolve) => setTimeout(resolve, time))
 }
