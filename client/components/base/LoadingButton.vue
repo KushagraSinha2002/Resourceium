9df7a27f518acfdb0d20fffafbdcec213ac93e7f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-16 10:50:03 +0550

    feat(LoadingButton.vue): add disabled prop

--- client/components/base/LoadingButton.vue
+++ client/components/base/LoadingButton.vue
@@ -1,7 +1,8 @@
 <template>
   <button
     :id="`loading-button__${text.toLowerCase()}`"
-    class="flex justify-center py-2 space-x-3 transition duration-300 transform rounded-15px hover:scale-105 sm:py-3 focus:outline-none bg-primary ring-black ring-1"
+    class="flex justify-center py-2 space-x-3 transition duration-300 transform rounded-15px hover:scale-105 sm:py-3 focus:outline-none bg-primary ring-black ring-1 disabled:bg-gray-700"
+    :disabled="disabled"
   >
     <div v-if="!loading" class="text-white">{{ text }}</div>
     <ig-icon
@@ -20,10 +21,13 @@ export default {
       type: String,
       required: true,
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
     loading: {
       type: Boolean,
       default: false,
-      // required: true,
     },
   },
 }
