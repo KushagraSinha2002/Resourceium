80f65417500267e7a9c0166479c5daa591bd8eee
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-28 09:01:22 +0550

    feat(Header.vue): display buttons conditionally

--- client/components/page/index/Header.vue
+++ client/components/page/index/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex-none w-full py-3 bg-[#F5F5F7] flex">
     <div
-      class="flex items-center justify-between w-full px-4 mx-auto sm:w-9/12 2xl:w-8/12"
+      class="flex items-center justify-between w-full px-6 mx-auto sm:w-9/12 2xl:w-8/12"
     >
       <NuxtLink
         id="link-faq"
@@ -10,21 +10,35 @@
       >
         Why Resourceium?
       </NuxtLink>
-      <div class="flex items-center space-x-3 font-semibold sm:space-x-5">
-        <NuxtLink
-          id="link-login"
-          class="text-primary font-poppins hover:underline"
-          :to="{ name: 'accounts-login' }"
+      <div>
+        <div
+          v-if="!$auth.loggedIn"
+          class="flex items-center space-x-3 font-semibold sm:space-x-5"
         >
-          Sign In
-        </NuxtLink>
-        <NuxtLink
-          id="link-register"
-          class="text-primary font-poppins hover:underline"
-          :to="{ name: 'accounts-register' }"
-        >
-          Sign Up
-        </NuxtLink>
+          <NuxtLink
+            id="link-login"
+            class="text-primary font-poppins hover:underline"
+            :to="{ name: 'accounts-login' }"
+          >
+            Sign In
+          </NuxtLink>
+          <NuxtLink
+            id="link-register"
+            class="text-primary font-poppins hover:underline"
+            :to="{ name: 'accounts-register' }"
+          >
+            Sign Up
+          </NuxtLink>
+        </div>
+        <div v-else>
+          <NuxtLink
+            id="link-dashboard"
+            class="text-primary font-poppins hover:underline"
+            :to="{ name: 'dashboard' }"
+          >
+            Dashboard
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </div>
