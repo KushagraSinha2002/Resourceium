a3bd3c9ed076108d42a78085501e94dafa6c07d5
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-31 14:06:42 +0000

    fix repository

--- server/src/main/java/com/bitlegion/server/uploads/DocumentRepository.java
+++ server/src/main/java/com/bitlegion/server/uploads/DocumentRepository.java
@@ -2,24 +2,13 @@ package com.bitlegion.server.uploads;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 public interface DocumentRepository extends CrudRepository<Document, Integer> {
     public Optional<Document> findByStorageID(Long storageID);
 
-    //@Query("SELECT * FROM DOCUMENT a where a.dateOfUpload = :dateOfUpload")
-    //public Optional<Document> findbyDateofupload(@Param("dateOfUpload") ArrayList dateOfUpload);
+    public Optional<Document> findByDateOfUpload(Long dateOfUpload);
 
-    public Optional<Document> findbyDateofUpload(Long dateOfUpload);
-
-    // @Query("SELECT a FROM Document a where a.storageID = :storageID AND
-    // a.account.id =
-    // :userId")
-    // public Optional<Document> findBySlugAndUserID(@Param("storageID") String
-    // storageID,
-    // @Param("userId") Integer userId);
 }
