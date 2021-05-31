775695e007a6ba8bf479d18c6978eb4cd7deb0f5
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-31 15:50:19 +0550

    feat(FolderRepository.java): add method to query by title and account

--- server/src/main/java/com/bitlegion/server/uploads/FolderRepository.java
+++ server/src/main/java/com/bitlegion/server/uploads/FolderRepository.java
@@ -1,14 +1,19 @@
 package com.bitlegion.server.uploads;
 
 import java.util.Collection;
+import java.util.List;
 
 import com.bitlegion.server.accounts.Account;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface FolderRepository extends CrudRepository<Folder, Integer> {
-    @Query("SELECT f FROM Folder f WHERE f.account = :account")
-    public Collection<Folder> findByAccount(@Param("account") Account account);
+    // @Query("SELECT f FROM Folder f WHERE f.account = :account")
+    // public Collection<Folder> findByAccount(@Param("account") Account account);
+
+    public Collection<Folder> findByAccount(Account account);
+
+    public Collection<Folder> findByAccountAndTitle(Account account, String title);
+
+    public List<Folder> findByAccountOrderByLastEditedDesc(Account account);
 }
