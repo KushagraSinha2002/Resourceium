8e5f669a2000f4ffe6c4bd58c7e2ff1d7369525f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-01 00:05:50 +0550

    feat(EmptyData.vue): component to handle no folders

---
+++ client/components/page/folders/EmptyData.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center w-full h-full">
+    <div class="relative p-5 space-y-5 text-center lg:w-3/5">
+      <div class="text-3xl font-styled-code">*CRICKET NOISES*</div>
+      <div class="text-xl font-styled">
+        <span>Seems like you have not created any folders.</span>
+        <span>Click on the button on the bottom right to create one.</span>
+      </div>
+    </div>
+  </div>
+</template>
