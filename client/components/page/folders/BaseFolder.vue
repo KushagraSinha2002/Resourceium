a6a31f1cc15360e3b42461bd5dfe491221720840
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-01 00:04:22 +0550

    feat(BaseFolder.vue): add tooltips for folder info

--- client/components/page/folders/BaseFolder.vue
+++ client/components/page/folders/BaseFolder.vue
@@ -1,38 +1,77 @@
 <template>
   <NuxtLink
     :id="`folder-${folder.id}`"
+    v-tippy="{ placement: 'top', arrow: true, animation: 'shift-toward' }"
     :to="{
       name: 'folders-folderId',
       params: { folderId: folder.id },
     }"
-    class="relative flex flex-col items-center shadow-lg cv-folder-container"
+    :content="getFolderDetails"
+    class="flex flex-col items-center"
   >
-    <FontAwesomeIcon
-      :icon="['fas', 'ellipsis-v']"
-      class="absolute h-4 text-black transition duration-500 transform bg-transparent cursor-pointer hover:text-gray-800 md:h-6 right-3 md:top-4 top-3 md:right-4 hover:scale-110 cv-folder-control"
-    ></FontAwesomeIcon>
-    <div class="flex flex-none w-full p-2 bg-gray-200 md:p-4 rounded-t-15px">
-      <ig-icon
-        no-size
-        no-color
-        name="folder"
-        class="h-16 mx-auto sm:h-24 text-amber-500"
-      ></ig-icon>
-    </div>
-    <div
-      class="flex-grow w-full p-2 text-center rounded-b-md bg-denim-300 line-clamp-2 font-poppins cv-title"
-    >
-      {{ folder.title }}
+    <div class="rounded-15px hover:ring-2">
+      <div class="flex justify-center md:p-4">
+        <img
+          :src="require('~/assets/images/folder-icon.png')"
+          :alt="`folder-${folder.id}`"
+        />
+        <div
+          class="z-20 flex items-center justify-center w-8 h-8 mt-auto mb-2 -ml-2 transition duration-500 transform rounded-full cursor-pointer hover:bg-black hover:bg-opacity-40"
+          @click.stop.prevent="handleMenuOpen"
+        >
+          <img :src="require('~/assets/images/open-menu.png')" class="h-6" />
+        </div>
+      </div>
+      <div
+        class="p-2 text-center line-clamp-1 justify-self-end rounded-b-md font-poppins"
+      >
+        {{ folder.title }}
+      </div>
     </div>
   </NuxtLink>
 </template>
 
 <script>
+import LocalizedFormat from 'dayjs/plugin/localizedFormat'
+
 export default {
   props: {
     folder: {
       required: true,
       type: Object,
+    },
+  },
+  data() {
+    return {
+      disabled: false,
+    }
+  },
+  computed: {
+    getFolderDetails() {
+      const folder = this.folder
+      let template = '<ol style="text-align: left">'
+      template += `
+        <li>Files: ${folder.documents.length}</li>
+        <li>Uploaded: ${this.formatDate(folder.dateOfUpload)}</li>
+        <li>Updated: ${this.formatDate(folder.lastEdited)}</li>
+      `
+      if (folder.tags.length > 0) {
+        template += `
+          <li>${folder.tags.join(', ')}</li>
+        `
+      }
+      template += '</ol>'
+      return template
+    },
+  },
+  methods: {
+    handleMenuOpen() {
+      // event.stopPropagation()
+      console.log('here')
+    },
+    formatDate(date) {
+      this.$dayjs.extend(LocalizedFormat)
+      return this.$dayjs(date).format('l')
     },
   },
 }
