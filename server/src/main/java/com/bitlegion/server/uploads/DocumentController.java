7f62e8129aa0c3f7ea626b5a530b145e93a3d2d2
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-01 22:14:08 +0550

    feat(DocumentController.java): add endpoint to delete document

--- server/src/main/java/com/bitlegion/server/uploads/DocumentController.java
+++ server/src/main/java/com/bitlegion/server/uploads/DocumentController.java
@@ -2,6 +2,7 @@ package com.bitlegion.server.uploads;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.bitlegion.server.accounts.Account;
 import com.bitlegion.server.accounts.AccountRepository;
@@ -67,6 +69,27 @@ public class DocumentController {
             userRepository.save(account);
             folderRepository.save(folder);
             return ResponseEntity.status(HttpStatus.OK).body(document);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{fileID}")
+    public ResponseEntity<Document> uploadFile(HttpServletRequest request, @PathVariable Integer fileID) {
+        try {
+            tokenChecker.checkAndReturnTokenOrRaiseException(request);
+            Optional<Document> maybeDocument = documentRepository.findById(fileID);
+            if (maybeDocument.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            } else {
+                Document document = maybeDocument.get();
+                Folder folder = document.getFolder();
+                folder.removeDocument(document);
+                documentRepository.delete(document);
+                folderRepository.save(folder);
+                return ResponseEntity.status(HttpStatus.OK).body(document);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return ResponseEntity.badRequest().build();
