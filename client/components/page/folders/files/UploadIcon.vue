392654d105ec2f106e7702e86320e6f8bded450f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-01 14:43:53 +0550

    feat(UploadIcon.vue): component to handle uploading files

---
+++ client/components/page/folders/files/UploadIcon.vue
@@ -0,0 +1,122 @@
+<template>
+  <form enctype="multipart/form-data" novalidate @submit.prevent="handleSubmit">
+    <h1>Upload files</h1>
+    <div class="dropbox">
+      <input
+        id="file"
+        ref="inputBox"
+        type="file"
+        name="file"
+        :disabled="isSaving"
+        @change="handleFileChange"
+      />
+      <button type="submit" class="bg-blue-300 border border-black">
+        Upload
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import filesize from 'filesize'
+import { sleep } from '~/utils/sleep.js'
+
+const STATUS_INITIAL = 0
+const STATUS_SAVING = 1
+const STATUS_SUCCESS = 2
+const STATUS_FAILED = 3
+
+export default {
+  filters: {
+    getFileSize(value) {
+      return filesize(value)
+    },
+  },
+  props: {
+    folder: { type: Object, required: true },
+  },
+  data() {
+    return {
+      currentStatus: null,
+      file: null,
+    }
+  },
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED
+    },
+  },
+  mounted() {
+    this.reset()
+  },
+  methods: {
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL
+      this.$refs.inputBox.value = null
+      this.file = null
+    },
+    save(formData) {
+      // upload data to the server
+      this.currentStatus = STATUS_SAVING
+      this.$axios
+        .$post(`/folders/upload-file/${this.folder.id}`, formData)
+        .then((_resp) => {
+          this.$addAlert({
+            message: 'Uploaded file successfully',
+            type: 'success',
+            timeOut: 1000,
+          })
+        })
+        .catch((_err) => {
+          this.$addAlert({
+            message: 'Error in uploading file',
+            type: 'danger',
+            timeOut: 1000,
+          })
+        })
+      this.currentStatus = STATUS_SUCCESS
+    },
+    handleFileChange() {
+      this.file = this.$refs.inputBox.files[0]
+    },
+    async handleSubmit() {
+      // handle file changes
+      const formData = new FormData()
+
+      if (!this.file) {
+        this.$addAlert({
+          message: 'Please select a file to upload',
+          type: 'info',
+          timeOut: 1000,
+        })
+        return
+      }
+      if (this.file.size > 2048000) {
+        this.$addAlert({
+          message: 'File size can not be above 2 MB',
+          type: 'danger',
+          timeOut: 1000,
+        })
+        this.reset()
+        return
+      }
+      formData.append('file', this.file, this.file.name)
+      // save it
+      this.save(formData)
+      this.reset()
+      await sleep(500)
+      this.$emit('refreshFolder')
+    },
+  },
+}
+</script>
