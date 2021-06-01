17ec801f359a11dfee7d733d54cf9b86ec248da3
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-01 14:42:38 +0550

    fix(AccountRepository.java): remove generic query strings

--- server/src/main/java/com/bitlegion/server/accounts/AccountRepository.java
+++ server/src/main/java/com/bitlegion/server/accounts/AccountRepository.java
@@ -2,18 +2,11 @@ package com.bitlegion.server.accounts;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
 
 public interface AccountRepository extends CrudRepository<Account, Integer> {
-    @Query("SELECT a FROM Account a where a.email = :email")
-    public Optional<Account> findByEmail(@Param("email") String email);
+    public Optional<Account> findByEmail(String email);
 
-    @Query("SELECT a FROM Account a where a.username = :username")
-    public Optional<Account> findByUsername(@Param("username") String username);
+    public Optional<Account> findByUsername(String username);
 
 }
