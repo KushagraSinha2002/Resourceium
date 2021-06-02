1701f6dcbc88f1e3f056f984ebf1c75929e10d43
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-02 15:28:40 +0550

    feat(EmptyData.vue): move file

---
+++ client/components/common/EmptyData.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="px-5 space-y-5 text-center lg:w-3/5">
+    <div class="text-3xl font-styled-code">*CRICKET NOISES*</div>
+    <div class="text-xl font-styled">
+      <slot>Nothing found here!</slot>
+    </div>
+  </div>
+</template>
