28dd12a6f185ebee0dfb483da2f8f0d93882ea29
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-02 15:29:52 +0550

    fix(GridItem.vue): complete styling

--- client/components/page/folders/files/GridItem.vue
+++ client/components/page/folders/files/GridItem.vue
@@ -1,9 +1,11 @@
 <template>
   <div
     :id="`file-${file.id}`"
+    v-tippy="{ placement: 'top', arrow: true, animation: 'shift-toward' }"
     class="flex flex-col w-10/12 p-2 mx-auto bg-gray-100 ring-2 sm:ring-0 sm:w-full hover:ring-2 rounded-15px"
+    :content="getFileDetails"
   >
-    <div class="flex justify-between w-full">
+    <div class="flex justify-between flex-none w-full">
       <a
         class="flex items-center justify-center rounded-full cursor-pointer hover:bg-black hover:bg-opacity-20 h-9 w-9"
         :href="getFileUrl"
@@ -19,17 +21,22 @@
         <ig-icon name="trash" variant="danger"></ig-icon>
       </div>
     </div>
-    <div class="flex justify-center">
+    <div
+      class="flex flex-col items-center justify-center flex-grow border-b border-black border-dashed"
+    >
       <img
         :src="require(`~/assets/images/extension/${getFileIcon}`)"
         :alt="`file-${file.id} icon`"
         class="inline p-3 w-28 h-28"
       />
+      <div
+        class="w-full font-mono text-sm text-center truncate"
+        style="width: 25ch"
+      >
+        {{ file.name }}
+      </div>
     </div>
-    <div class="w-full font-mono text-sm text-center border-b border-black">
-      {{ file.name }}
-    </div>
-    <div class="py-2">
+    <div class="flex-none py-2">
       <div>
         <span class="text-sm">Uploaded:</span>
         <span class="font-semibold font-styled-code">
@@ -71,6 +78,15 @@ export default {
     },
     getFileDateOfUpload() {
       return this.$dayjs(this.file.dateOfUpload).format('D MMM, H:mm')
+    },
+    getFileDetails() {
+      const file = this.file
+      let template = '<ol style="text-align: left">'
+      template += `
+        <li>Name: <strong>${file.name}</strong></li>
+      `
+      template += '</ol>'
+      return template
     },
     getFileIcon() {
       for (let i = 0; i < EXTENSIONS.length; i++) {
