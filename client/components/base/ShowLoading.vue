5bc8fb7c45bf4f34d60e0f392bbc2afe76d78016
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-04 23:07:22 +0550

    fix(ShowLoading.vue): adjust to new layout

--- client/components/base/ShowLoading.vue
+++ client/components/base/ShowLoading.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="absolute inset-0 flex items-center justify-center">
-    <div class="p-10 rounded-lg sm:ml-64">
-      <div class="sk-folding-cube">
-        <div class="sk-cube1 sk-cube"></div>
-        <div class="sk-cube2 sk-cube"></div>
-        <div class="sk-cube4 sk-cube"></div>
-        <div class="sk-cube3 sk-cube"></div>
-      </div>
-    </div>
+  <div class="sk-folding-cube">
+    <div class="sk-cube1 sk-cube"></div>
+    <div class="sk-cube2 sk-cube"></div>
+    <div class="sk-cube4 sk-cube"></div>
+    <div class="sk-cube3 sk-cube"></div>
   </div>
 </template>
 
