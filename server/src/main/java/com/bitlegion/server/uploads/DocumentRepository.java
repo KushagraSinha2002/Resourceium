f7f8f7638e2778ad3ef3cd8d66d102f28aa5077f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-04 20:22:27 +0550

    fix(DocumentRepository.java): set correct method name

--- server/src/main/java/com/bitlegion/server/uploads/DocumentRepository.java
+++ server/src/main/java/com/bitlegion/server/uploads/DocumentRepository.java
@@ -1,16 +1,15 @@
 package com.bitlegion.server.uploads;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
 public interface DocumentRepository extends CrudRepository<Document, Integer> {
     public Optional<Document> findByStorageID(Long storageID);
 
-    public Optional<Document> findByDateOfUpload(Long dateOfUpload);
+    public List<Document> findAllByDateOfUpload(Date dateOfUpload);
 
     public List<Document> findByFolder(Folder folder);
 }
