ace9e9f27b9636bd9d40eef80b1406c0a7839671
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-04 22:29:55 +0550

    feat(DocumentRepository.java): method to find by folders account

--- server/src/main/java/com/bitlegion/server/uploads/DocumentRepository.java
+++ server/src/main/java/com/bitlegion/server/uploads/DocumentRepository.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import com.bitlegion.server.accounts.Account;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface DocumentRepository extends CrudRepository<Document, Integer> {
     public Optional<Document> findByStorageID(Long storageID);
 
     public List<Document> findAllByDateOfUpload(Date dateOfUpload);
+
+    public List<Document> findByFolderAccount(Account account);
 
     public List<Document> findByFolder(Folder folder);
 }
