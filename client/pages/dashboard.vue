ef9e7f0f2bbf7ff3efff83ab59b4788b36b9ee8b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-05 09:33:46 +0550

    fix(dashboard.vue): add padding on screens

--- client/pages/dashboard.vue
+++ client/pages/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lg:px-3 xl:px-10 2xl:px-32">
-    <page-dashboard-stats></page-dashboard-stats>
+    <page-dashboard-stats class="my-5"></page-dashboard-stats>
     <page-dashboard-graph-one></page-dashboard-graph-one>
   </div>
 </template>
