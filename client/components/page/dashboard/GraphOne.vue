ccb6e29b9f97ce89e5740dab88ba58a8c3717cec
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-05 09:33:11 +0550

    feat(GraphOne.vue): styling of chart

--- client/components/page/dashboard/GraphOne.vue
+++ client/components/page/dashboard/GraphOne.vue
@@ -12,6 +12,7 @@ export default {
   data() {
     return {
       graphData: null,
+      maxFiles: 0,
       options: {
         responsive: true,
         maintainAspectRatio: false,
@@ -20,19 +21,29 @@ export default {
           display: true,
           text: `Files uploaded by ${this.$auth.user.username}`,
           fontSize: 24,
-          fontColor: '#6b7280',
+          fontColor: '#120201',
+          fontFamily: 'Oxygen Mono',
         },
         tooltips: {
-          backgroundColor: '#17BF62',
+          backgroundColor: '#7527a8',
         },
         scales: {
+          xAxes: [
+            {
+              ticks: {
+                autoSkip: false,
+                maxRotation: 70,
+                minRotation: 70,
+                fontSize: 20,
+              },
+            },
+          ],
           yAxes: [
             {
               ticks: {
                 beginAtZero: false,
-                steps: 10,
-                stepValue: 5,
-                max: 100,
+                max: this.maxFiles,
+                fontSize: 15,
               },
             },
           ],
@@ -43,11 +54,18 @@ export default {
   async fetch() {
     const response = await this.$axios.$get('/core/dashboard/graph/1')
     this.graphData = {
-      labels: Object.keys(response).map((x) =>
-        this.$dayjs(x).format('DD MMM, YYYY')
-      ),
-      datasets: [{ data: Object.values(response) }],
+      labels: Object.keys(response).map((x) => this.$dayjs(x).format('DD-MMM')),
+      datasets: [
+        {
+          data: Object.values(response),
+          backgroundColor: 'rgba(169, 209, 232, 0.4)',
+          borderCapStyle: 'round',
+          borderJoinStyle: 'round',
+          label: 'Files uploaded',
+        },
+      ],
     }
+    this.maxFiles = Math.max.apply(Math, Object.values(response))
   },
 }
 </script>
