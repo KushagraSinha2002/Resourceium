00a4b62a1141469c4c7737def7aec5b830947139
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-06 23:00:49 +0550

    feat(FolderTagger.vue): component to handle creating new tags

--- client/components/page/folders/files/FolderTagger.vue
+++ client/components/page/folders/files/FolderTagger.vue
@@ -1,52 +1,76 @@
 <template>
   <div
-    class="flex justify-between px-2 pt-2 pb-1 border border-gray-400 rounded-tl-15px rounded-br-15px"
+    class="px-2 pt-2 pb-2 border border-gray-400 rounded-tl-15px rounded-br-15px"
   >
-    <div>
-      {{ tags }}
-      <div v-for="tag in tags" :key="tag.id">{{ tag }}</div>
-    </div>
-    <div class="flex flex-col items-center">
-      <v-swatches v-model="colour" show-fallback></v-swatches>
+    <div class="grid grid-cols-2 gap-3 sm:grid-cols-3">
       <div
-        class="flex items-center justify-center w-6 h-6 rounded-full cursor-pointer hover:bg-black hover:bg-opacity-20"
-        title="Add Tag"
-        @click.stop.prevent="addTag"
+        v-for="tag in tags"
+        :key="tag.id"
+        class="flex items-center space-x-2"
       >
-        <ig-icon
-          name="plus"
-          variant="success"
-          size="sm"
-          class="!block mx-auto"
-        ></ig-icon>
+        <div
+          v-tippy
+          class="w-10 h-10 rounded-lg"
+          :style="{ backgroundColor: tag.colour }"
+          :content="getTagDetails(tag.id)"
+        ></div>
+        <div>{{ tag.name }}</div>
       </div>
     </div>
+    <form
+      v-if="tags.length < 6"
+      class="flex items-center space-x-3"
+      @submit.prevent="addTag"
+    >
+      <v-swatches v-model="colour" show-fallback class="mt-1"></v-swatches>
+      <input
+        v-model="name"
+        type="text"
+        class="block rounded-md appearance-none ring focus:outline-none"
+        required
+      />
+      <button
+        type="submit"
+        class="font-semibold text-purple-800 underline font-poppins"
+      >
+        Add Tag
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    folderID: { type: Number, required: true },
+    folderId: { type: Number, required: true },
   },
   data() {
     return {
       colour: '#A463BF',
-      name: 'Hello',
-      tags: null,
+      name: '',
+      tags: [],
+      newTag: null,
     }
   },
   async fetch() {
-    this.tags = await this.$axios.$get(`/tags/get/${this.folderID}`)
+    this.tags = await this.$axios.$get(`/tags/folder/${this.folderId}`)
   },
   methods: {
+    getTagDetails(index) {
+      const tag = this.tags.find((tag) => tag.id === index)
+      let template = '<ol style="text-align: left">'
+      template += `
+        <li>Name: ${tag.name}</li>
+      `
+      template += '</ol>'
+      return template
+    },
     async addTag() {
-      // await console.log(`/tags/create/${this.folder.id}`)
-      const resp = await this.$axios.$post(`/tags/create/${this.folderID}`, {
+      await this.$axios.$post(`/tags/create/${this.folderId}`, {
         name: this.name,
         colour: this.colour,
       })
-      console.log(resp)
+      this.$fetch()
     },
   },
 }
