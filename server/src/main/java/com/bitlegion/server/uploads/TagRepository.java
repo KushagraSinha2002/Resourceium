61d22dd4a1a4e5ae5572034c53a9c114721fa1aa
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-06 22:59:13 +0550

    feat(TagRepository.java): add methods to get tags for endpoints

--- server/src/main/java/com/bitlegion/server/uploads/TagRepository.java
+++ server/src/main/java/com/bitlegion/server/uploads/TagRepository.java
@@ -1,10 +1,13 @@
 package com.bitlegion.server.uploads;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface TagRepository extends CrudRepository<Tag, Integer> {
 
-     public Collection<Tag> findByNameAndColour(String name, String colour);
+     public Optional<Tag> findByName(String name);
+
+     public Collection<Folder> findAllByFolders(Folder folder);
 }
