a6c28d45c38264f717a91f673bca614834294fbd
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-07 11:16:11 +0550

    feat(AccountRepository.java): add method to get accounts by username

--- server/src/main/java/com/bitlegion/server/accounts/AccountRepository.java
+++ server/src/main/java/com/bitlegion/server/accounts/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.bitlegion.server.accounts;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -8,5 +9,7 @@ public interface AccountRepository extends CrudRepository<Account, Integer> {
     public Optional<Account> findByEmail(String email);
 
     public Optional<Account> findByUsername(String username);
+
+    public Collection<Account> findAllByUsernameContainingIgnoreCase(String username);
 
 }
