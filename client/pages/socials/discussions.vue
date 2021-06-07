2b3448d7d00866f79589d776b823fb265c26655a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-07 12:48:43 +0550

    feat(discussions.vue, index.vue): rename file

--- client/pages/socials/discussions.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>All discussions</div>
-</template>
---
+++ client/pages/socials/discussions/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <page-socials-discussions-index-container></page-socials-discussions-index-container>
+  </div>
+</template>
