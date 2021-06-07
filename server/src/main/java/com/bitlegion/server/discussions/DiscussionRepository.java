93246429e5fd6a52c006f15e469d57d1c0a2c825
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-07 22:24:45 +0550

    feat(DiscussionRepository.java): add method to find discussion by name

--- server/src/main/java/com/bitlegion/server/discussions/DiscussionRepository.java
+++ server/src/main/java/com/bitlegion/server/discussions/DiscussionRepository.java
@@ -1,5 +1,6 @@
 package com.bitlegion.server.discussions;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface DiscussionRepository extends CrudRepository<Discussion, Integer> {
 
     public Optional<Discussion> findByNameIgnoreCase(String name);
+
+    public Collection<Discussion> findAllByNameContainingIgnoreCase(String name);
 
 }
