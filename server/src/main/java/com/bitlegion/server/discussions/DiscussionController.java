657c1dc015252cfbad756c5f6aeb373d9e7b3331
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-07 12:53:52 +0550

    feat(DiscussionController.java): handle correct relationships being set up

--- server/src/main/java/com/bitlegion/server/discussions/DiscussionController.java
+++ server/src/main/java/com/bitlegion/server/discussions/DiscussionController.java
@@ -3,6 +3,7 @@ package com.bitlegion.server.discussions;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +44,14 @@ public class DiscussionController {
         return discussionRepository.findAll();
     }
 
+    @GetMapping("/account/{accountID}")
+    public @ResponseBody Iterable<Discussion> getListDiscussionsByAccount(HttpServletRequest request,
+            @PathVariable Integer accountID) {
+        Optional<Account> maybeAccount = accountRepository.findById(accountID);
+        Account account = maybeAccount.get();
+        return account.getDiscussions();
+    }
+
     @PostMapping("/create")
     public @ResponseBody ResponseEntity<?> createDiscussion(HttpServletRequest request,
             @RequestBody HashMap<String, String> reqBody) {
@@ -56,15 +66,17 @@ public class DiscussionController {
             discussion.setCreatedBy(token.getAccount());
             discussion.setName(name);
             discussion.setDescription(description);
+            discussionRepository.save(discussion);
             String stringifiedUserIDs = reqBody.get("userIDs");
             if (stringifiedUserIDs != null) {
                 List<Integer> ids = Arrays.stream(stringifiedUserIDs.split(",")).map(Integer::parseInt)
                         .collect(Collectors.toList());
-                System.out.println(ids);
                 Iterable<Account> accounts = accountRepository.findAllById(ids);
-                accounts.forEach(acc -> discussion.addAccount(acc));
+                accounts.forEach(acc -> {
+                    acc.addDiscussion(discussion);
+                    accountRepository.save(acc);
+                });
             }
-            discussionRepository.save(discussion);
             return ResponseEntity.status(HttpStatus.OK).body(discussion);
         } catch (Exception e) {
             System.out.println(e.getMessage());
