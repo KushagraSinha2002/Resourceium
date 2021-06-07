5dfe21178642627a764becff63e9a6980ddcee10
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-07 22:06:15 +0550

    feat(Header.vue): show user image

--- client/components/sidebar/Header.vue
+++ client/components/sidebar/Header.vue
@@ -3,9 +3,9 @@
     class="flex items-center px-5 py-2 mb-5 space-x-4 border-[#C4C4C4] border bg-[#F5F5F7]"
   >
     <img
-      :src="require('~/assets/images/id_logo.png')"
+      :src="$auth.user.imageURL"
       alt="profile image"
-      class="w-12 h-12"
+      class="w-12 h-12 rounded-3xl"
     />
 
     <div class="leading-none truncate font-styled-code">
@@ -13,9 +13,6 @@
         {{ $auth.user.firstName }}
       </div>
       <div>{{ $auth.user.lastName }}</div>
-      <!-- <div class="text-xs text-warm-gray-700 font-poppins">
-        {{ $auth.user.email }}
-      </div> -->
     </div>
   </div>
 </template>
