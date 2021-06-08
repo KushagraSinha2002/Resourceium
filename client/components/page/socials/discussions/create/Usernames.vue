aa944697a8ae44953755c040252ed7d746c5013d
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-08 11:25:53 +0550

    feat(Usernames.vue): component to display usernames form input

---
+++ client/components/page/socials/discussions/create/Usernames.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label
+      class="text-sm font-bold tracking-wider uppercase font-poppins"
+      for="autosuggest-input"
+    >
+      {{ label }}
+    </label>
+    <page-socials-discussions-create-username-input
+      @selected="handleSelect"
+    ></page-socials-discussions-create-username-input>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { type: String, required: true },
+  },
+  methods: {
+    handleSelect(val) {
+      this.$emit('handleSelect', val)
+    },
+  },
+}
+</script>
