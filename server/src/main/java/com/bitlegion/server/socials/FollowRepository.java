860371b8a9903fcf0c14a9be09f26dd0cdb1f30f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-08 13:11:51 +0550

    feat(FollowRepository.java): add methods to get followers and following

--- server/src/main/java/com/bitlegion/server/socials/FollowRepository.java
+++ server/src/main/java/com/bitlegion/server/socials/FollowRepository.java
@@ -1,7 +1,18 @@
 package com.bitlegion.server.socials;
 
+import java.util.Collection;
+import java.util.Optional;
+
+import com.bitlegion.server.accounts.Account;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface FollowRepository extends CrudRepository<Follow, Integer> {
+
+    public Optional<Follow> findByFollowerAndFollowing(Account follower, Account following);
+
+    public Collection<Follow> findAllByFollowing(Account following);
+
+    public Collection<Follow> findAllByFollower(Account follower);
 
 }
