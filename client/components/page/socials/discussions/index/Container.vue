24a62ecf43cd898bd314363e6a841c1924eae9f8
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-09 11:21:50 +0550

    feat(Container.vue): component to handle all discussions

---
+++ client/components/page/socials/discussions/index/Container.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="px-3 my-5 md:px-10">
+    <div
+      class="text-3xl text-center text-true-gray-700 font-poppins md:text-5xl"
+    >
+      All your discussions
+    </div>
+    <div class="grid grid-cols-1 gap-6 mt-5 lg:grid-cols-2 2xl:grid-cols-3">
+      <page-socials-discussions-index-card
+        v-for="discussion in discussions"
+        :key="discussion.id"
+        :discussion="discussion"
+      ></page-socials-discussions-index-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      discussions: [],
+    }
+  },
+  async fetch() {
+    const id = this.$auth.user.id
+    this.discussions = await this.$axios.$get(`/discussions/account/${id}`)
+  },
+}
+</script>
