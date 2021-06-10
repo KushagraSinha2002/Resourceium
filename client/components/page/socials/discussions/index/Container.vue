4a50dd965bc49354bc892ce5b3945822e82f5fae
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-10 08:48:12 +0550

    feat(Container.vue): add create button and empty data

--- client/components/page/socials/discussions/index/Container.vue
+++ client/components/page/socials/discussions/index/Container.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="px-3 my-5 md:px-10">
-    <div
-      class="text-3xl text-center text-true-gray-700 font-poppins md:text-5xl"
-    >
-      All your discussions
+  <div class="flex flex-col h-full px-3">
+    <page-socials-discussions-index-create-icon
+      class="fixed z-20 bottom-3 right-3"
+      @refreshFolders="$fetch()"
+    ></page-socials-discussions-index-create-icon>
+    <div v-if="discussions.length > 0" class="h-full">
+      <div
+        class="flex-none my-5 text-3xl text-center md:px-10 text-true-gray-700 font-poppins md:text-5xl"
+      >
+        All your discussions
+      </div>
+      <div class="grid grid-cols-1 gap-6 mt-5 lg:grid-cols-2 2xl:grid-cols-3">
+        <page-socials-discussions-index-card
+          v-for="discussion in discussions"
+          :key="discussion.id"
+          :discussion="discussion"
+        ></page-socials-discussions-index-card>
+      </div>
     </div>
-    <div class="grid grid-cols-1 gap-6 mt-5 lg:grid-cols-2 2xl:grid-cols-3">
-      <page-socials-discussions-index-card
-        v-for="discussion in discussions"
-        :key="discussion.id"
-        :discussion="discussion"
-      ></page-socials-discussions-index-card>
+    <div v-else class="flex items-center justify-center flex-grow">
+      <common-empty-data>
+        <span>Seems like you have not joined any discussions.</span>
+        <span>Click on the button on the bottom right to create/join one.</span>
+      </common-empty-data>
     </div>
   </div>
 </template>
