f5583af644b76bb139f3cf8b792f3c4cf4f2f86f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-10 09:06:56 +0550

    feat: add method to find by folder and account

--- server/src/main/java/com/bitlegion/server/socials/FavoriteRepository.java
+++ server/src/main/java/com/bitlegion/server/socials/FavoriteRepository.java
@@ -1,7 +1,14 @@
 package com.bitlegion.server.socials;
 
+import java.util.Optional;
+
+import com.bitlegion.server.accounts.Account;
+import com.bitlegion.server.uploads.Folder;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface FavoriteRepository extends CrudRepository<Favorite, Integer> {
+
+    public Optional<Favorite> findByFolderAndAccount(Folder folder, Account account);
 
 }
