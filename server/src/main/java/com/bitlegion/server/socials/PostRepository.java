47e359bc121d2c3ef6ae762f470fba5e21701b10
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-11 21:17:32 +0550

    feat(PostRepository.java): method to count num posts of a folder

--- server/src/main/java/com/bitlegion/server/socials/PostRepository.java
+++ server/src/main/java/com/bitlegion/server/socials/PostRepository.java
@@ -1,7 +1,11 @@
 package com.bitlegion.server.socials;
 
+import com.bitlegion.server.uploads.Folder;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Integer> {
+
+    public Integer countByFolder(Folder folder);
 
 }
