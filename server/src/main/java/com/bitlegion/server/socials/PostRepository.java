7b75ed59fa95abf477185ad49da7e3b7f768f045
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-12 08:42:24 +0550

    feat(PostRepository.java): add method to limit queries

--- server/src/main/java/com/bitlegion/server/socials/PostRepository.java
+++ server/src/main/java/com/bitlegion/server/socials/PostRepository.java
@@ -1,11 +1,17 @@
 package com.bitlegion.server.socials;
 
+import java.util.List;
+
+import com.bitlegion.server.discussions.Discussion;
 import com.bitlegion.server.uploads.Folder;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends CrudRepository<Post, Integer> {
+public interface PostRepository extends JpaRepository<Post, Integer> {
 
     public Integer countByFolder(Folder folder);
+
+    public List<Post> findAllByDiscussion(Discussion discussion, Pageable pageable);
 
 }
