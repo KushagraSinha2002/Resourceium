7e9e48b4f609b13ce48a1d03d5645d6fa72792eb
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-12 08:10:00 +0550

    feat(DiscussionRepository.java): add method to count by account

--- server/src/main/java/com/bitlegion/server/discussions/DiscussionRepository.java
+++ server/src/main/java/com/bitlegion/server/discussions/DiscussionRepository.java
@@ -3,6 +3,8 @@ package com.bitlegion.server.discussions;
 import java.util.Collection;
 import java.util.Optional;
 
+import com.bitlegion.server.accounts.Account;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface DiscussionRepository extends CrudRepository<Discussion, Integer> {
@@ -10,5 +12,7 @@ public interface DiscussionRepository extends CrudRepository<Discussion, Integer
     public Optional<Discussion> findByNameIgnoreCase(String name);
 
     public Collection<Discussion> findAllByNameContainingIgnoreCase(String name);
+
+    public Integer countByAccounts(Account account);
 
 }
