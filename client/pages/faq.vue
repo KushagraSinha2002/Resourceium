0c71aab85b41291d6c95626b9eedef9a30caff77
Tanishka-Garg <tan27peace@gmail.com>
2021-06-15 12:42:19 +0000

    faq

--- client/pages/faq.vue
+++ client/pages/faq.vue
@@ -1,5 +1,44 @@
 <template>
   <div>FAQs</div>
+  <div>------why resourcium------------
+resourcium allows you to store, search, and access files with a team. 
+Even if members leave, the files stay in place so your team can keep sharing information and work anywhere,
+from any device.
+The biggest reason for utilizing resourecium is to increase the visibility of your content.
+
+
+------Resetting your password-------
+To change the password for your account when you're already logged in, click "your account" in the sidebar.
+Update your password by clicking the pen icon in the Password section , change the passwordand click "update.”
+
+------Account Activity---------
+To review recent activity on your account, go to "dashboard" at the top there are 4 graphs representing your account activity.
+Here you'll find the list of files that you have uploaded over the past 7 days.
+
+-----Dashboard Preferences-------
+The "Best First" setting gives you an algorithmic dashboard. Your dashboard will show you the files resourcium thinks you'll 
+like, rather than just the newest content.
+
+------Tagging your posts---------
+Tags make it easier for readers to find posts about a specific topic on your blog. For instance, you could tag your 
+physics notes with #physics or posts about corona with #covid19.
+Tagging your posts is as easy as you’d expect. When you create a post, enter the tags in the field at the bottom of the post form
+
+------Update Profile-------
+To update basic profile information like name, username and email address you need to go to the "your account" option (10th position in the sidebar) 
+then click the option of "Update Account Details".There you can update all the information you want to change and then hit the "Update button".
+
+-------Making a post--------
+Ready to share something? Good!
+Nothing is quite so invigorating as a fresh post
+at the "upload" page (2nd position in the sidebar), you can click the option of "new folder".There you can add the title, description, and body of 
+the folder in which you can upload the desired files.
+
+------groups-------
+in the "groups" page you can create groups with some certain people and the posts shared on the particular group will be restricted to the respective 
+members of that group. for instance , to share physics resource with few people only you can create a group with them and share the resource in that group.
+
+</div>
 </template>
 
 <script>
