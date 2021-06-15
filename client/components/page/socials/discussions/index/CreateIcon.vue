b2d383882682c30c73a1b825bae97364c44a0dec
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-15 18:49:33 +0550

    fix(CreateIcon.vue): add invite to label

--- client/components/page/socials/discussions/index/CreateIcon.vue
+++ client/components/page/socials/discussions/index/CreateIcon.vue
@@ -3,6 +3,6 @@
     :to="{ name: 'socials-discussions-create' }"
     class="px-5 py-3 text-lg text-gray-200 transition-transform transform shadow-2xl cursor-pointer rounded-15px bg-primary font-poppins hover:-translate-x-0.5 hover:-translate-y-0.5 hover:shadow-inner"
   >
-    Create
+    Join/Create
   </NuxtLink>
 </template>
