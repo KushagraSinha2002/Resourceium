da08d9dfc713c224912482f47d89926113dfd764
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-25 23:05:35 +0550

    feat(Container.vue, Tabs.vue): add support for favorite folders

--- client/components/page/folders/Container.vue
+++ client/components/page/folders/Container.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="relative flex w-full h-full">
+  <div class="relative flex flex-col w-full h-full py-3">
     <page-folders-create-folder-icon
-      class=""
       @refreshFolders="fetchFolders()"
     ></page-folders-create-folder-icon>
-    <div v-if="!$fetchState.pending" class="w-full h-full">
+    <page-folders-tabs
+      v-model="selectedTab"
+      :possible="possibleTabs"
+    ></page-folders-tabs>
+    <div v-if="!loading" class="w-full mt-2">
       <div
         v-if="folders.length > 0"
         class="grid grid-cols-2 gap-4 px-2 py-5 sm:gap-6 bg-cream-white sm:px-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-7 auto-rows-max"
@@ -34,15 +37,41 @@
 export default {
   data() {
     return {
+      selectedTab: 'All',
+      possibleTabs: ['All', 'Favorites'],
       folders: [],
+      loading: false,
     }
   },
   async fetch() {
+    this.loading = true
     await this.fetchFolders()
+    this.loading = false
+  },
+  watch: {
+    async selectedTab() {
+      this.loading = true
+      if (this.selectedTab === 'Favorites') {
+        await this.fetchFavorites()
+      } else if (this.selectedTab === 'All') {
+        await this.fetchFolders()
+      }
+      this.loading = false
+    },
   },
   methods: {
     async fetchFolders() {
-      const resp = await this.$axios.$get(`/folders/user-folders`)
+      const resp = await this.$axios.$get('/folders/user-folders')
+      resp.map((element) => {
+        element.dateOfUpload = this.$dayjs(element.dateOfUpload)
+        element.lastEdited = this.$dayjs(element.lastEdited)
+        return element
+      })
+      this.folders = resp
+      this.selectedTab = 'All'
+    },
+    async fetchFavorites() {
+      const resp = await this.$axios.$get('/folders/user-favorite-folders')
       resp.map((element) => {
         element.dateOfUpload = this.$dayjs(element.dateOfUpload)
         element.lastEdited = this.$dayjs(element.lastEdited)
---
+++ client/components/page/folders/Tabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-around w-full text-xl md:text-3xl font-poppins">
+    <div
+      v-for="(tab, index) in possible"
+      :key="index"
+      class="px-3 py-1 transition-shadow duration-300 cursor-pointer rounded-15px"
+      :class="{ 'ring-2 ring-rose-600/60': value === tab }"
+      @click="selectOption(tab)"
+    >
+      {{ tab }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: String, required: true },
+    possible: { type: Array, required: true },
+  },
+  methods: {
+    selectOption(tab) {
+      this.$emit('input', tab)
+    },
+  },
+}
+</script>
