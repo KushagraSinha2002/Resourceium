b5935fa3e3238efdc57da6e08645f1a2cb7e190c
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-25 20:08:17 +0550

    feat(ShowLoading.vue): change animation

--- client/components/base/ShowLoading.vue
+++ client/components/base/ShowLoading.vue
@@ -1,82 +1,56 @@
 <template>
-  <div class="sk-folding-cube">
-    <div class="sk-cube1 sk-cube"></div>
-    <div class="sk-cube2 sk-cube"></div>
-    <div class="sk-cube4 sk-cube"></div>
-    <div class="sk-cube3 sk-cube"></div>
+  <div class="h-32">
+    <div class="loader">Loading...</div>
   </div>
 </template>
 
 <style scoped>
-.sk-folding-cube {
-  height: 80px;
+.loader,
+.loader::before,
+.loader::after {
+  animation: load1 1s infinite ease-out;
+  background: #2e2929;
+  height: 4em;
+  width: 1em;
+}
+
+.loader {
+  animation-delay: -0.16s;
+  color: #2e2929;
+  font-size: 11px;
   margin: 20px auto;
   position: relative;
-  transform: rotateZ(45deg);
-  width: 80px;
+  text-indent: -9999em;
+  transform: translateZ(0);
 }
 
-.sk-folding-cube .sk-cube {
-  float: left;
-  height: 50%;
-  position: relative;
-  transform: scale(1.1);
-  width: 50%;
-}
-
-.sk-folding-cube .sk-cube::before {
-  animation: sk-foldCubeAngle 2.4s infinite linear both;
-  background-color: hsl(273, 51%, 38%);
+.loader::before,
+.loader::after {
   content: '';
-  height: 100%;
-  left: 0;
   position: absolute;
   top: 0;
-  transform-origin: 100% 100%;
-  width: 100%;
 }
 
-.sk-folding-cube .sk-cube2 {
-  transform: scale(1.1) rotateZ(90deg);
+.loader::before {
+  animation-delay: -0.32s;
+  left: -1.5em;
 }
 
-.sk-folding-cube .sk-cube3 {
-  transform: scale(1.1) rotateZ(180deg);
+.loader::after {
+  left: 1.5em;
 }
 
-.sk-folding-cube .sk-cube4 {
-  transform: scale(1.1) rotateZ(270deg);
-}
-
-.sk-folding-cube .sk-cube2::before {
-  animation-delay: 0.3s;
-}
-
-.sk-folding-cube .sk-cube3::before {
-  animation-delay: 0.6s;
-}
-
-.sk-folding-cube .sk-cube4::before {
-  animation-delay: 0.9s;
-}
-
-@keyframes sk-foldCubeAngle {
+@keyframes load1 {
   0%,
-  10% {
-    opacity: 0;
-    transform: perspective(140px) rotateX(-180deg);
-  }
-
-  25%,
-  75% {
-    opacity: 1;
-    transform: perspective(140px) rotateX(0deg);
-  }
-
-  90%,
+  80%,
   100% {
-    opacity: 0;
-    transform: perspective(140px) rotateY(180deg);
+    box-shadow: 0 0;
+    height: 4em;
+  }
+
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
   }
 }
 </style>
